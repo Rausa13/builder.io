d6019beb8f8b7c716f5e759d31cbe51fe986e6d3
Alex Rausa <alex@konnected.asia>
2020-09-07 18:38:01 +0900

    First commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: uncomment next line and enter your own public API key
-  // builderAPIKey: '<YOUR PUBLIC API KEU>'
+  builderAPIKey: '34ab51055bf84ac7a5823bb96fda4c6c'
 };
